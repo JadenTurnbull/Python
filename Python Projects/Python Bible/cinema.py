4117fff31178602f639439ee0d557fd80f75bd9e
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-04-18 21:52:49 +0200

    final commit

--- Python Projects/Python Bible/cinema.py
+++ Python Projects/Python Bible/cinema.py
@@ -9,6 +9,7 @@ while True:
 
     choice = input("What film would you like to watch?: ").strip().title()
 
+
     if choice in films:
         age = int(input("How old are you?: ").strip())
 
@@ -18,7 +19,6 @@ while True:
             # check enough seats
 
             num_seats = films[choice][1]
-
             if num_seats > 0:
                 print("Enjoy the film!")
                 films[choice][1] = films[choice][1] - 1
@@ -26,6 +26,8 @@ while True:
                 print("Sorry, we are sold out!")
         else:
             print("You are too young to see that film!")
-    else:
+    else: 
         print("We don't have that film...")
+
+        
     
